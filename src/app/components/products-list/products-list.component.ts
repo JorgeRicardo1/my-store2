78a7cda281bf87d1e9ec98655c00b5ebb5f59780
Jorge <jorge.ricardo@utp.edu.co>
2023-10-23 13:18:22 -0500

    curso finalizado

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,61 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../Models/product.model'
+import { StoreService } from '../../services/store.service'
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent {
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/asianStyle1.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/asianStyle2.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/asianStyle3.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/asianStyle4.jpg'
-    },
-    {
-      id: '5',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/asianStyle5.jpg'
-    },
-    {
-      id: '6',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/asianStyle6.jpg'
-    },
-    {
-      id: '7',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/asianStyle7.jpg'
-    },
-    {
-      id: '8',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/asianStyle8.jpg'
-    },
+export class ProductsListComponent implements OnInit {
 
-  ];
+  myShoppingCart : Product[] = [];
+  total = 0;
+  products: Product[] = [];
+
+  today = new Date();
+  date = new Date(2021, 1, 2);
+
+  constructor(
+    private storeService: StoreService,
+    private producsService: ProductsService
+  ){
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
+  ngOnInit(): void {
+    this.producsService.getAllProducts()
+    .subscribe(data => {
+      this.products = data;
+    })
+  }
+
+  onAddToShoppingCart(product: Product){
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
+  }
 }
